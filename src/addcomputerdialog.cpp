b3e91a340dbc388eeae39d8c3119898ff7e4c9e9
f0rw4rd-dev <52598385+f0rw4rd-dev@users.noreply.github.com>
2023-01-06 13:24:30 +0500

    .

--- src/addcomputerdialog.cpp
+++ src/addcomputerdialog.cpp
@@ -1,14 +1,86 @@
 #include "addcomputerdialog.h"
 #include "ui_addcomputerdialog.h"
+#include "regularexpressions.h"
+
+#include <QRegularExpressionValidator>
 
 AddComputerDialog::AddComputerDialog(QWidget *parent) :
     QDialog(parent),
     ui(new Ui::AddComputerDialog)
 {
     ui->setupUi(this);
+
+    setWindowTitle("Добавить сборку ПК");
+
+    // Init pointers to UI elements
+    _buttonAddComputer = findChild<QPushButton*>("buttonAddComputer");
+    _inputCPUId = findChild<QLineEdit*>("inputCPUId");
+    _inputCPUName = findChild<QLineEdit*>("inputCPUName");
+    _inputMotherboardId = findChild<QLineEdit*>("inputMotherboardId");
+    _inputMotherboardName = findChild<QLineEdit*>("inputMotherboardName");
+    _inputVideocardId = findChild<QLineEdit*>("inputVideocardId");
+    _inputVideocardName = findChild<QLineEdit*>("inputVideocardName");
+    _inputRAMId = findChild<QLineEdit*>("inputRAMId");
+    _inputRAMName = findChild<QLineEdit*>("inputRAMName");
+    _inputRAMAmount = findChild<QLineEdit*>("inputRAMAmount");
+    _inputCaseId = findChild<QLineEdit*>("inputCaseId");
+    _inputCaseName = findChild<QLineEdit*>("inputCaseName");
+    _inputPSUId = findChild<QLineEdit*>("inputPSUId");
+    _inputPSUName = findChild<QLineEdit*>("inputPSUName");
+    _inputHDDId = findChild<QLineEdit*>("inputHDDId");
+    _inputHDDName = findChild<QLineEdit*>("inputHDDName");
+    _inputHDDAmount = findChild<QLineEdit*>("inputHDDAmount");
+    _inputSSDId = findChild<QLineEdit*>("inputSSDId");
+    _inputSSDName = findChild<QLineEdit*>("inputSSDName");
+    _inputSSDAmount = findChild<QLineEdit*>("inputSSDAmount");
+    _inputSSDM2Id = findChild<QLineEdit*>("inputSSDM2Id");
+    _inputSSDM2Name = findChild<QLineEdit*>("inputSSDM2Name");
+    _inputSSDM2Amount = findChild<QLineEdit*>("inputSSDM2Amount");
+    _inputFanId = findChild<QLineEdit*>("inputFanId");
+    _inputFanName = findChild<QLineEdit*>("inputFanName");
+    _inputFanAmount = findChild<QLineEdit*>("inputFanAmount");
+    _inputWCSId = findChild<QLineEdit*>("inputWCSId");
+    _inputWCSName = findChild<QLineEdit*>("inputWCSName");
+    _inputCoolerId = findChild<QLineEdit*>("inputCoolerId");
+    _inputCoolerName = findChild<QLineEdit*>("inputCoolerName");
+
+    // Validators
+    _inputCPUId->setValidator(new QRegularExpressionValidator(RegularExpressions::digit, this));
+    _inputMotherboardId->setValidator(new QRegularExpressionValidator(RegularExpressions::address, this));
+    _inputVideocardId->setValidator(new QRegularExpressionValidator(RegularExpressions::address, this));
+    _inputRAMId->setValidator(new QRegularExpressionValidator(RegularExpressions::digit, this));
+    _inputRAMAmount->setValidator(new QRegularExpressionValidator(RegularExpressions::digit, this));
+    _inputCaseId->setValidator(new QRegularExpressionValidator(RegularExpressions::digit, this));
+    _inputPSUId->setValidator(new QRegularExpressionValidator(RegularExpressions::digit, this));
+    _inputHDDId->setValidator(new QRegularExpressionValidator(RegularExpressions::digit, this));
+    _inputHDDAmount->setValidator(new QRegularExpressionValidator(RegularExpressions::digit, this));
+    _inputSSDId->setValidator(new QRegularExpressionValidator(RegularExpressions::digit, this));
+    _inputSSDAmount->setValidator(new QRegularExpressionValidator(RegularExpressions::digit, this));
+    _inputSSDM2Id->setValidator(new QRegularExpressionValidator(RegularExpressions::digit, this));
+    _inputSSDM2Amount->setValidator(new QRegularExpressionValidator(RegularExpressions::digit, this));
+    _inputFanId->setValidator(new QRegularExpressionValidator(RegularExpressions::digit, this));
+    _inputFanAmount->setValidator(new QRegularExpressionValidator(RegularExpressions::digit, this));
+    _inputWCSId->setValidator(new QRegularExpressionValidator(RegularExpressions::digit, this));
+    _inputCoolerId->setValidator(new QRegularExpressionValidator(RegularExpressions::digit, this));
+
+    // Connections
+    //connect(_inputId, &QLineEdit::textChanged, this, &SetWarehouseDialog::loadWarehouse);
+    connect(_buttonAddComputer, &QPushButton::clicked, this, &AddComputerDialog::addComputer);
 }
 
 AddComputerDialog::~AddComputerDialog()
 {
     delete ui;
 }
+
+void AddComputerDialog::loadCpu(const QString &id)
+{
+
+}
+
+
+void AddComputerDialog::addComputer()
+{
+
+}
+
--- src/addcomputerdialog.h
+++ src/addcomputerdialog.h
@@ -2,6 +2,7 @@
 #define ADDCOMPUTERDIALOG_H
 
 #include <QDialog>
+#include <QLineEdit>
 
 namespace Ui {
 class AddComputerDialog;
@@ -14,6 +15,54 @@ class AddComputerDialog : public QDialog
 public:
     explicit AddComputerDialog(QWidget *parent = nullptr);
     ~AddComputerDialog();
+
+private slots:
+    void loadCpu(const QString &id);
+    void addComputer();
+
+private:
+    QPushButton *_buttonAddComputer;
+
+    QLineEdit *_inputCPUId;
+    QLineEdit *_inputCPUName;
+
+    QLineEdit *_inputMotherboardId;
+    QLineEdit *_inputMotherboardName;
+
+    QLineEdit *_inputVideocardId;
+    QLineEdit *_inputVideocardName;
+
+    QLineEdit *_inputRAMId;
+    QLineEdit *_inputRAMName;
+    QLineEdit *_inputRAMAmount;
+
+    QLineEdit *_inputCaseId;
+    QLineEdit *_inputCaseName;
+
+    QLineEdit *_inputPSUId;
+    QLineEdit *_inputPSUName;
+
+    QLineEdit *_inputHDDId;
+    QLineEdit *_inputHDDName;
+    QLineEdit *_inputHDDAmount;
+
+    QLineEdit *_inputSSDId;
+    QLineEdit *_inputSSDName;
+    QLineEdit *_inputSSDAmount;
+
+    QLineEdit *_inputSSDM2Id;
+    QLineEdit *_inputSSDM2Name;
+    QLineEdit *_inputSSDM2Amount;
+
+    QLineEdit *_inputFanId;
+    QLineEdit *_inputFanName;
+    QLineEdit *_inputFanAmount;
+
+    QLineEdit *_inputWCSId;
+    QLineEdit *_inputWCSName;
+
+    QLineEdit *_inputCoolerId;
+    QLineEdit *_inputCoolerName;
 
 private:
     Ui::AddComputerDialog *ui;
